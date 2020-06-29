1837eec6c0ff6dcd67b736116268b99b2f429fc8
Bruno Bertechini <Bruno.Bertechini@outlook.com>
2020-06-29 13:26:13 +1000

    added args to build.sh & Dockerfiles for Ubuntu-18.04

--- build.sh
+++ build.sh
@@ -1,10 +1,68 @@
 #!/bin/bash
+VALID_TARGETS="base python docker dotnet nodejs"
+DockerHubPrefix=""
+ImageName="devopsubuntu"
+Tag="latest"
+AppendMode="image"
+DryRun=0
+targets=($VALID_TARGETS)
+
+usage() {
+  echo "Usage:"
+  echo "    -h  Display this help message."
+  echo "    -p  Docker Image Preffix (docker hub username). Default: empty/null"
+  echo "    -i  Docker Image name. Default: $ImageName + OS_VERSION"
+  echo "    -t  Docker Tag to be used. Default: latest."
+  echo "    -m  Append Mode - tag or image. Default: image."
+  echo "    -b  Build Targets - Comma separated list of Targets to build. Ex: -b base,dotnet,nodejs"
+  echo "    -x  Dry run -- do not actually build the images."
+  exit 0
+}
+
+validateTargets() {
+  echo "TODO: Validate targets"
+}
+
+while getopts "hp:i:t:m:b:x" opt; do
+  case ${opt} in
+    h)
+      usage
+      ;;
+    m) AppendMode="$OPTARG"
+    ;;
+    p) DockerHubPrefix="$OPTARG"
+    ;;
+    i) ImageName="$OPTARG"
+    ;;
+    t) Tag="$OPTARG"
+    ;;
+    b) 
+      OIFS=$IFS;
+      IFS=",";
+      targets=($OPTARG)
+      IFS=$OIFS;
+      ;;
+    x) DryRun=1
+    ;;
+    \?) 
+      echo "Invalid option -$OPTARG" >&2
+      usage
+      ;;
+  esac
+done
 
 print_info() {
   lightgreen='\e[92m'
   nocolor='\033[0m'
   echo -e "${lightgreen}[*] $1${nocolor}"
 }
+
+print_info "Building all images..."
+print_info "- DockerHubPrefix: '$DockerHubPrefix'"
+print_info "- ImageName: '$ImageName'"
+print_info "- Tag: '$Tag'"
+print_info "- AppendMode: '$AppendMode'"
+echo -e "\e[92m[*] - BuildTargets: ${targets[@]}\033[0m"
 
 print_info "Making all bash scripts executable"
 
@@ -13,44 +71,39 @@ find . -type f -iname "*.sh" -exec chmod +x {} \;
 # Select the version you prefer, or both (16.04 18.04)
 for VERSION in 18.04
 do
+  if [[ ! -z $DockerHubPrefix ]]; then
+    ImageName="$DockerHubPrefix/$ImageName"
+  else
+    ImageName="$ImageName$VERSION"
+  fi
 
-  print_info "Building base image"
+  for target in ${targets[@]}; do
 
-  cd ubuntu$VERSION-base
+    IMG=""
+    TAG=""
 
-  docker build -t devopsubuntu$VERSION:latest .
+    # Check AppendMode and use target on image_name or tag
+    if [[ "$AppendMode" = "image" ]]; then
+      IMG="$ImageName-${target}"
+      TAG="${Tag}"
+    elif [[ "$AppendMode" = "tag" ]];then
+      if [[ "$Tag" = "latest" ]]; then
+        IMG="$ImageName"
+        TAG=":${target}"
+      else
+        IMG="$ImageName"
+        TAG=":${target}-${Tag}"
+      fi
+    fi
 
-  cd ..
+    print_info "Building $IMG... (docker build -t $IMG:$TAG .)"
+    if [[ $DryRun -eq 0 ]]; then
+      cd ubuntu$VERSION-$target
+      pwd
+      docker build -t $IMG . --build-arg IMAGE=$IMG --build-arg TAG=$TAG
+      cd ..
+    fi
 
-  print_info "Building Python image"
+  done
 
-  cd ubuntu$VERSION-python
-
-  docker build -t devopsubuntu$VERSION-python:latest .
-
-  cd ..
-
-  print_info "Building Docker inception image"
-
-  cd ubuntu$VERSION-docker
-
-  docker build -t devopsubuntu$VERSION-docker:latest .
-
-  cd ..
-
-  print_info "Building .NET Core image"
-
-  cd ubuntu$VERSION-dotnet
-
-  docker build -t devopsubuntu$VERSION-dotnet:latest .
-
-  cd ..
-
-  print_info "Building Node.js image"
-
-  cd ubuntu$VERSION-nodejs
-
-  docker build -t devopsubuntu$VERSION-nodejs:latest .
-
-  cd ..
 done
